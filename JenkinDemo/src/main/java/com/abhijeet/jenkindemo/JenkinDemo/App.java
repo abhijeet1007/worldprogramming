57b329faa290e704cea3bfcc6dc668ec37520d51
Abhi <Abhi@Hadoop>
2017-11-12 11:38:50 +0530

    this is my first program

---
+++ JenkinDemo/src/main/java/com/abhijeet/jenkindemo/JenkinDemo/App.java
@@ -0,0 +1,13 @@
+package com.abhijeet.jenkindemo.JenkinDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
